0387a62c6bc492adfbf6d34829033a53f299891e
Vuthuru Srivardhan <vuthurusrivardhan@gmail.com>
2024-06-23 22:32:36 +0530

    made signup route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,36 @@
 import { Hono } from 'hono';
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { sign } from 'hono/jwt';
 
-const app = new Hono();
+const app = new Hono<{
+  Bindings:{
+    DATABASE_URL:string,
+    JWT_SECRET_MESSAGE:string
+  }
+}>();
 
-app.post('/api/v1/signup', (c) => {
-	return c.text('signup route')
+app.post('/api/v1/signup',async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+  const body = await c.req.json();
+  const user = await prisma.user.create({
+    data:{
+      email: body.email,
+      password: body.password
+    }
+  })
+  if(!user){
+    c.status(403);
+    return c.json({
+      error: "error while signup"
+    })
+  }
+  const token = await sign({id: user.id}, c.env.JWT_SECRET_MESSAGE);
+	return c.json({
+    JWT: token
+  })
 })
 
 app.post('/api/v1/signin', (c) => {
