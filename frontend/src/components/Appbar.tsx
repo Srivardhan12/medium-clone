a960807c82ad4a6400f6f806e657d779365027e9
Srivardhan24 <108692789+Srivardhan12@users.noreply.github.com>
2025-07-22 11:13:44 +0530

    Update Appbar.tsx
    Changed the Main name of the app

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -39,7 +39,7 @@ export const Appbar = () => {
                 className="text-2xl font-bold cursor-pointer"
                 onClick={() => navigate("/")}
             >
-                Bloger
+                Blogger
             </div>
             <div className="flex items-center gap-4">
                 {user ? (
